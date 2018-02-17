0e40cd2012eb7b555531c39cb981fb77b04366a3
Mario Bermudez Jr <Mario.Bermudez.Jr@outlook.com>
2018-02-16 20:54:59 -0800

    refactor: Fetch the categories within the root view and use the capitalize function

--- src/containers/RootView.js
+++ src/containers/RootView.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 import Title from '../components/common/Title';
 import { fetchCategories, fetchCategoryPosts } from '../actions';
+import { capitalize } from '../utils/helpers';
 
 // Class to handle the organization of each component, this is one of two screen/routes
 class RootView extends Component {
   componentWillMount() {
-    fetchCategories();
+    this.props.fetchCategories();
   }
-  render() {
-    console.log('PROPS: ', this.props.posts);
 
-    console.log('Good Afternoon sir.');
+  renderCategory(category) {
+    return (
+      <div key={category.name} className={category.name}>
+        <Link to={`/${category.path}>`}>{capitalize(category.name)}</Link>
+      </div>
+    );
+  }
+
+  render() {
+    const { categories } = this.props;
+
     return (
       <div className="list-books">
         <Title name="Root View" />
+        {/* _props.map(prop => this.props.updateField(prop, props[prop])); */}
+        {_.map(categories, category => this.renderCategory(category))}
         <div className="post-detail">
           <Link to="/postDetail">Post Detail</Link>
         </div>
@@ -31,6 +43,6 @@ class RootView extends Component {
   }
 }
 
-const mapStateToProps = ({ posts }) => ({ posts });
+const mapStateToProps = ({ categories }) => ({ categories });
 
 export default connect(mapStateToProps, { fetchCategories, fetchCategoryPosts })(RootView);
