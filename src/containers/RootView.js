03b1796fcf11adb04cf70c4fb81f5cc615015d17
Mario Bermudez Jr <Mario.Bermudez.Jr@outlook.com>
2018-04-07 15:06:18 -0700

    refactor: Remove unnecessary imports and comment on a state that is going to be used for the category titles in the root view

--- src/containers/RootView.js
+++ src/containers/RootView.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import _ from 'lodash';
 
 import Title from '../components/common/Title';
-import Button from '../components/common/Buttons';
 import Buttons from '../components/common/Buttons';
 import { fetchCategories, fetchCategoryPosts } from '../actions';
 import { capitalize } from '../utils/helpers';
-
-const btnWidth = { width: '100px' };
-const defaultStyle = 'btn btn-outline-secondary mr-2';
-const activeStyle = 'btn btn-danger mr-2';
 
 // Class to handle the organization of each component, this is one of two screen/routes
 class RootView extends Component {
@@ -40,6 +34,7 @@ class RootView extends Component {
 
   render() {
     const { categories } = this.props;
+    console.log('Category: ', categories);
 
     return (
       <div className="list-categories">
