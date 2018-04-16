7273c9cb20fa89235ab6e7ec6ccdb88e29c770b6
Mario Bermudez Jr <Mario.Bermudez.Jr@outlook.com>
2018-04-15 18:11:00 -0700

    refactor: Move the main button headers to the root file so all links carry it around

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // Local imports
+import Title from './components/common/Title';
+import HeaderButtons from './components/HeaderButtons';
 import RootView from './containers/RootView';
 import CategoryView from './containers/CategoryView';
 import PostDetailView from './containers/PostDetailView';
@@ -11,11 +13,29 @@ import CreateEditView from './containers/CreateEditView';
 import './App.css';
 
 class ReadableApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isChecked: true };
+  }
+  toggleChange = () => {
+    this.setState({
+      isChecked: !this.state.isChecked
+    });
+  };
+
   render() {
     return (
       <BrowserRouter>
         <div>
-          {/* <Header title="Readable" /> */}
+          <Title name="Readable" />
+          <HeaderButtons />
+          <div>
+            <a style={{ fontSize: 15 }}> Sort By </a>
+            <input type="checkbox" checked={this.state.isChecked} onChange={this.toggleChange} />
+            <a style={{ fontSize: 15 }}> Date </a>
+            <input type="checkbox" checked={!this.state.isChecked} onChange={this.toggleChange} />
+            <a style={{ fontSize: 15 }}> Score </a>
+          </div>
           <Switch>
             <Route exact path="/" component={RootView} />
             <Route pexact path="/category" component={CategoryView} />
--- src/containers/RootView.js
+++ src/containers/RootView.js
@@ -1,60 +1,16 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import Title from '../components/common/Title';
-import HeaderButtons from '../components/HeaderButtons';
 import { fetchCategories, fetchCategoryPosts } from '../actions';
-import { capitalize } from '../utils/helpers';
 
 // Class to handle the organization of each component, this is one of two screen/routes
 class RootView extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isChecked: true };
-  }
-
-  toggleChange = () => {
-    this.setState({
-      isChecked: !this.state.isChecked
-    });
-  };
-
   componentWillMount() {
     this.props.fetchCategories();
   }
 
-  renderCategory(category) {
-    return (
-      <div key={category.name} className={category.name}>
-        <Link to={`/${category.path}>`}>{capitalize(category.name)}</Link>
-      </div>
-    );
-  }
-
   render() {
-    const { categories } = this.props;
-    console.log('Category: ', categories);
-
-    return (
-      <div className="App">
-        <Title name="Readable" />
-        <HeaderButtons />
-
-        {/* <Buttons name="All" />
-          {typeof categories !== 'undefined' && categories.length > 0
-            ? categories.map(({ name }) => <Buttons key={name} name={name} />)
-            : null}
-        </div> */}
-        <div>
-          <a style={{ fontSize: 15 }}> Sort By </a>
-          <input type="checkbox" checked={this.state.isChecked} onChange={this.toggleChange} />
-          <a style={{ fontSize: 15 }}> Date </a>
-          <input type="checkbox" checked={!this.state.isChecked} onChange={this.toggleChange} />
-          <a style={{ fontSize: 15 }}> Score </a>
-        </div>
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
